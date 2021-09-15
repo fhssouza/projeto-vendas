b9e8f2ae0fff4689f3652e948453457246a3a593
fhssouza <fhssouza@gmail.com>
2021-09-15 19:41:59 -0300

    Iniciando o Projeto

---
+++ backend/produtos-ms/src/test/java/tech4me/com/produtos/ProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package tech4me.com.produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
