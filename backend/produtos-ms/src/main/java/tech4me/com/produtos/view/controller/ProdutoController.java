74c918bacf61f4e10124c7e6de7f288967f5808a
fhssouza <fhssouza@gmail.com>
2021-09-15 21:35:03 -0300

    Criando Produto Controller

---
+++ backend/produtos-ms/src/main/java/tech4me/com/produtos/view/controller/ProdutoController.java
@@ -0,0 +1,26 @@
+package tech4me.com.produtos.view.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import tech4me.com.produtos.service.ProdutoService;
+import tech4me.com.produtos.shared.ProdutoDTO;
+
+@RestController
+@RequestMapping("api/produtos")
+public class ProdutoController {
+    @Autowired
+    ProdutoService servico;
+
+    @GetMapping
+    public ResponseEntity<List<ProdutoDTO>> listarProdutos(){
+        return new ResponseEntity<>(servico.listarProdutos(), HttpStatus.ACCEPTED);
+    }
+    
+}
