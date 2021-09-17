2a4b665784579d417581ef07ddff8074a6cdbe9a
fhssouza <fhssouza@gmail.com>
2021-09-17 17:10:40 -0300

    Criando o Workspace

---
+++ backend/venda-ms/src/test/java/tech4me/com/Venda/VendaApplicationTests.java
@@ -0,0 +1,13 @@
+package tech4me.com.Venda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
