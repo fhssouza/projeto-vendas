afcb03498c982decd82c85d118a71944587590e7
fhssouza <fhssouza@gmail.com>
2021-09-26 17:25:57 -0300

    Inclusão do VendaDTO

---
+++ backend/venda-ms/src/main/java/tech4me/com/venda/shared/VendaDTO.java
@@ -0,0 +1,37 @@
+package tech4me.com.venda.shared;
+
+import java.time.LocalDate;
+
+public class VendaDTO {
+    private LocalDate data;
+    private String produto;
+    private Integer quantidade;
+    private Double valor;
+    
+    public LocalDate getData() {
+        return data;
+    }
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+    public String getProduto() {
+        return produto;
+    }
+    public void setProduto(String produto) {
+        this.produto = produto;
+    }
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+    public Double getValor() {
+        return valor;
+    }
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    
+}
