ca8d3b3c9d981412772192c8277d146c1fe88a55
fhssouza <fhssouza@gmail.com>
2021-09-26 21:44:42 -0300

    Registrando o Produtos-ms no eureka-server

--- backend/produtos-ms/src/main/java/tech4me/com/produtos/ProdutosApplication.java
+++ backend/produtos-ms/src/main/java/tech4me/com/produtos/ProdutosApplication.java
@@ -2,8 +2,10 @@ package tech4me.com.produtos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProdutosApplication {
 
 	public static void main(String[] args) {
