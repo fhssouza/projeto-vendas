23c8da3ded3aa77d6eda0fda6968db835942067d
fhssouza <fhssouza@gmail.com>
2021-09-28 19:10:21 -0300

    Configurando o Hystrix

--- backend/produtos-ms/src/main/java/tech4me/com/produtos/ProdutosApplication.java
+++ backend/produtos-ms/src/main/java/tech4me/com/produtos/ProdutosApplication.java
@@ -3,11 +3,13 @@ package tech4me.com.produtos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableHystrix
 public class ProdutosApplication {
 
 	public static void main(String[] args) {
--- backend/produtos-ms/src/main/java/tech4me/com/produtos/clienteHTPP/VendasFeignClient.java
+++ backend/produtos-ms/src/main/java/tech4me/com/produtos/clienteHTPP/VendasFeignClient.java
@@ -1,15 +1,26 @@
 package tech4me.com.produtos.clienteHTPP;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import tech4me.com.produtos.shared.Venda;
 
-@FeignClient(name="venda-ms")
+@FeignClient(name="venda-ms", fallback = VendasFeignClientFallback.class)
 public interface VendasFeignClient {
     @GetMapping(path = "/api/vendas/{produto}/lista")
     List<Venda> obterVendas(@PathVariable String produto);
+}
+
+@Component
+class VendasFeignClientFallback implements VendasFeignClient {
+
+    @Override
+    public List<Venda> obterVendas(String produto){
+        return new ArrayList<>();
+    }
 }
